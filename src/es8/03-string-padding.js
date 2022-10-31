9275480746cd917416b56ceef9cce8055475febe
AnthonyCampo97_Pt <anthonycampo97@outlook.com>
2022-10-31 18:14:04 -0500

    Se agregan archivos a ES8

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,6 @@
+const string = 'hello';
+
+console.log(string.padStart(7, 'hi')); //Agrega caracter al principio de la cadena
+
+console.log(string.padEnd(6,'_')); //Agrega caracter al final de la cadena
+
---
+++ src/es8/04-trailing_commas.js
@@ -0,0 +1,4 @@
+const array = [12,25,63,45,12, , , ,14]; //designa un espacio en memoria en las posiones vacias del array
+
+console.log(array);
+console.log(array.length);
